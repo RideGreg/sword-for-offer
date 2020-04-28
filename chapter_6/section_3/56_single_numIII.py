6825a394350a3829895002dec32a219584e3ed69
Ming Greg <gregming@gmail.com>
2020-04-27 17:03:59 -0700

    Ming: update

--- chapter_6/section_3/56_single_numIII.py
+++ chapter_6/section_3/56_single_numIII.py
@@ -15,7 +15,22 @@
 # 也就是说二进制中至少有一位1，记为第n位。我们以第n位是否为1，把数组分为两个子数组。
 
 
-def singleNumber(nums: list) -> tuple:
+def singleNumber(nums: list[int]) -> tuple:
+    x = reduce(operator.__xor__, nums)
+    # get least significant bit. Another way is:
+    # lsb = 1
+    # while x & lsb == 0:
+    #     lsb <<= 1
+    lsb = x & (-x)
+    a = b = 0
+    for num in nums:
+        if num & lsb:
+            a ^= num
+        else:
+            b ^= num
+    return (a, b)
+
+def singleNumber_darkTianTian(nums: list) -> tuple:
     total_xor = get_xor(nums)
     mask = 1
     while total_xor & mask == 0:
@@ -28,3 +43,6 @@ def singleNumber(nums: list) -> tuple:
 def get_xor(nums: list) -> int:
     from functools import reduce
     return reduce(lambda x, y: x ^ y, nums)
+
+singleNumber([1,2,10,4,1,4,3,3]) # (2, 10)
+singleNumber([1,0,9,1]) # (0, 9)
